b9e20ed4cc472dd7cc588e6a783741757c2e4371
Nanda P Caetano <fernandamariacaetanopereira@gmail.com>
2023-08-16 11:40:38 -0300

    Add files via upload

---
+++ 16.08.2023/main.js
@@ -0,0 +1,8 @@
+// Variaveis
+var n1, n2, resultado;
+
+n1 = prompt("Digite o primeiro valor");
+n2 = prompt("Digite o segundo valor");
+
+resultado = parseFloat(n1) + parseFloat(n2);
+console.log(resultado);
